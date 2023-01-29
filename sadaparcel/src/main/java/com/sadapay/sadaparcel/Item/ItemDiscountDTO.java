867e3a980db487de9caeafe610eddb3a9e111297
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2023-01-29 14:15:02 +0500

    Added dto implementation for patch request

--- sadaparcel/src/main/java/com/sadapay/sadaparcel/Item/ItemDiscountDTO.java
+++ sadaparcel/src/main/java/com/sadapay/sadaparcel/Item/ItemDiscountDTO.java
@@ -1,4 +1,22 @@
 package com.sadapay.sadaparcel.Item;
 
 public class ItemDiscountDTO {
+    private boolean isDiscount;
+
+    public ItemDiscountDTO(){
+
+    }
+
+    public ItemDiscountDTO(boolean isDiscount) {
+        this.isDiscount = isDiscount;
+    }
+
+    public boolean getIsDiscount() {
+        return isDiscount;
+    }
+
+    public void setIsDiscount(boolean isDiscount) {
+        this.isDiscount = isDiscount;
+    }
 }
+
