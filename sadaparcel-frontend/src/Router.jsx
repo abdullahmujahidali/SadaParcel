8d7e2436493ced5fd17d245d456e001f07aea5e5
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2023-01-29 19:54:09 +0500

    configure router

--- sadaparcel-frontend/src/Router.jsx
+++ sadaparcel-frontend/src/Router.jsx
@@ -3,9 +3,34 @@ import React from 'react';
 import {Navigate} from 'react-router-dom';
 import {useRoutes} from 'react-router-dom';
 
+import Navbar from 'components/Layout/NavbarLayout';
+import NotFound from 'components/NotFound/NotFound';
+
+import SimpleLayout from 'components/Layout/NormalLayout';
+
+import ItemView from 'views/Item/ItemView';
+import OfferView from 'views/Offers/OfferView';
+
 
 export default function Router() {
   const routes = useRoutes([
+    {
+      path: '/',
+      element: <Navbar />,
+      children: [
+        {element: <Navigate to='/inventory' />, index: true},
+        {path: 'inventory', element: <ItemView />},
+        {path: 'offers', element: <OfferView />},
+      ],
+    },
+    {
+      element: <SimpleLayout />,
+      children: [
+        {element: <Navigate to='/inventory' />, index: true},
+        {path: '404', element: <NotFound />},
+        {path: '*', element: <Navigate to='/404' />},
+      ],
+    },
     {
       path: '*',
       element: <Navigate replace
