30fdcb7414ed41a430b67b02676494b5da3b535d
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2023-01-29 14:33:05 +0500

    If applied, this commit will... Add an ItemController class for the apis
    Why is this change needed?
    Prior to this change, there was no controller
    
    How does it address the issue?
    This change will add a basic request mapping and the controller for the application

---
+++ sadaparcel/src/main/java/com/sadapay/sadaparcel/Item/ItemController.java
@@ -0,0 +1,9 @@
+package com.sadapay.sadaparcel.Item;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="api/v1/item")
+public class ItemController {
+}
