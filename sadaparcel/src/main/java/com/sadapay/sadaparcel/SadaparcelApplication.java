2ed50bd5d368a2daef1b2e55765f5ff423721c45
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2023-01-29 13:46:19 +0500

    added sadaparcel template

---
+++ sadaparcel/src/main/java/com/sadapay/sadaparcel/SadaparcelApplication.java
@@ -0,0 +1,13 @@
+package com.sadapay.sadaparcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SadaparcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SadaparcelApplication.class, args);
+	}
+
+}
