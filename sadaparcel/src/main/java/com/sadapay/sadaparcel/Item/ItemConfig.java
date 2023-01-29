b2f88ad07e99315fcb1398350fb5d18c589699c6
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2023-01-29 14:30:03 +0500

    Added commandLineRunner for the Item package Prior to this change, there was no run resource for the repository
    This change will add the run method that will run after the context has loaded sucessfully

--- sadaparcel/src/main/java/com/sadapay/sadaparcel/Item/ItemConfig.java
+++ sadaparcel/src/main/java/com/sadapay/sadaparcel/Item/ItemConfig.java
@@ -1,4 +1,29 @@
 package com.sadapay.sadaparcel.Item;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
 public class ItemConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(ItemRepository itemRepository){
+        return args ->{
+            Item milk = new Item(
+                    "Apple",
+                    20,
+                    10.4
+            );
+            Item bread = new Item(
+                    "Banana",
+                    2,
+                    12.49
+            );
+
+            itemRepository.saveAll(
+                    List.of(milk, bread)
+            );
+        };
+    }
+
 }
