da12353dd159594dadc98b53a15516e970f78fc1
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2023-01-29 15:01:53 +0500

    Added list api and tests Prior to this change, there was no api and tests
    This change will add test and list app

--- sadaparcel/src/main/java/com/sadapay/sadaparcel/Item/ItemController.java
+++ sadaparcel/src/main/java/com/sadapay/sadaparcel/Item/ItemController.java
@@ -1,5 +1,6 @@
 package com.sadapay.sadaparcel.Item;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +10,20 @@ import java.util.List;
 @RestController
 @RequestMapping(path="api/v1/item")
 public class ItemController {
+    private final ItemService itemService;
+
+    @Autowired
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
 
     @GetMapping
-    public void getItems(){
+    public List<Item> getItems(){
+        return itemService.getItems();
 
     }
+
+
 
 }
--- sadaparcel/src/test/java/com/sadapay/sadaparcel/Item/ItemTest.java
+++
@@ -1,7 +0,0 @@
-package com.sadapay.sadaparcel.Item;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class ItemTest {
-
-}
