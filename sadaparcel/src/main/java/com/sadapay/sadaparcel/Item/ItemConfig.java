66abbff94045c4ebe263d73e4763f2f707f73782
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2023-01-29 18:13:48 +0500

    Added cors-header to connect to REACT app

--- sadaparcel/src/main/java/com/sadapay/sadaparcel/Item/ItemConfig.java
+++ sadaparcel/src/main/java/com/sadapay/sadaparcel/Item/ItemConfig.java
@@ -1,8 +1,12 @@
 package com.sadapay.sadaparcel.Item;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Collections;
 import java.util.List;
@@ -29,5 +33,17 @@ public class ItemConfig {
             );
         };
     }
-
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("http://127.0.0.1:5000"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new org.springframework.web.filter.CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
 }
