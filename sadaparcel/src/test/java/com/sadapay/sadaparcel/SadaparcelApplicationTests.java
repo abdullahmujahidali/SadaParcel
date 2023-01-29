84bf5675eab1a7a6da1dc6d5ff0f6ac1d950e058
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2023-01-29 14:45:53 +0500

    Added a test for list request

--- sadaparcel/src/test/java/com/sadapay/sadaparcel/SadaparcelApplicationTests.java
+++ sadaparcel/src/test/java/com/sadapay/sadaparcel/SadaparcelApplicationTests.java
@@ -1,10 +1,15 @@
 package com.sadapay.sadaparcel;
 
+import com.sadapay.sadaparcel.Item.Item;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SadaparcelApplicationTests {
@@ -17,6 +22,11 @@ class SadaparcelApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void givenItems_GetItemsEndpoint_ShouldReturnItemsList(){
+		String baseUrl = "http://localhost:" + port + "/api/v1/item";
 	}
 
 }
