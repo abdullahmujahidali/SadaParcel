5683b4206d539a97d105cb132a828b1ed552c2c6
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2023-01-29 19:20:26 +0500

    Update vite.config

--- sadaparcel-frontend/vite.config.ts
+++ sadaparcel-frontend/vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: '5000',
+  },
+  build: {
+    outDir: 'build',
+    rollupOptions: {
+      manualChunks: {
+        'react-feather': ['react-feather'],
+        'data-grid': ['@mui/x-data-grid'],
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      constants: path.resolve(__dirname, './src/constants'),
+      app: path.resolve(__dirname, './src/app'),
+      components: path.resolve(__dirname, './src/app/components'),
+      views: path.resolve(__dirname, './src/app/views'),
+      middlewares: path.resolve(__dirname, './src/middlewares'),
+      hooks: path.resolve(__dirname, './src/hooks'),
+      utilities: path.resolve(__dirname, './src/utilities'),
+    },
+  },
+});
