077ecef3a3cbed290169dc573c817ea7264204b3
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2023-01-29 14:40:46 +0500

    Added setup for the list api

--- sadaparcel/src/main/java/com/sadapay/sadaparcel/Item/ItemController.java
+++ sadaparcel/src/main/java/com/sadapay/sadaparcel/Item/ItemController.java
@@ -1,9 +1,18 @@
 package com.sadapay.sadaparcel.Item;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path="api/v1/item")
 public class ItemController {
+
+    @GetMapping
+    public void getItems(){
+
+    }
+
 }
--- sadaparcel/src/main/java/com/sadapay/sadaparcel/Item/ItemService.java
+++ sadaparcel/src/main/java/com/sadapay/sadaparcel/Item/ItemService.java
@@ -1,7 +1,14 @@
 package com.sadapay.sadaparcel.Item;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ItemService {
+    private final ItemRepository itemRepository;
+
+    @Autowired
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
 }
