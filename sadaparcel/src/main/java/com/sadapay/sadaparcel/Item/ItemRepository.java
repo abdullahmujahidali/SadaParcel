0271622075805dd04ae7f2ef4629e463b2995a91
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2023-01-29 14:03:51 +0500

    Added an item repository for the package Prior to this change, there was an empty repository interface
    This change will create a repository for performing CRUD operations on Item objects that are identified by UUID values in a database using JPA

--- sadaparcel/src/main/java/com/sadapay/sadaparcel/Item/ItemRepository.java
+++ sadaparcel/src/main/java/com/sadapay/sadaparcel/Item/ItemRepository.java
@@ -1,4 +1,10 @@
 package com.sadapay.sadaparcel.Item;
 
-public interface ItemRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, UUID> {
 }
